2c644ee95dac53c0f5218e5da097869529390d30
katy-the-kat <hello@is-a.space>
2024-11-25 11:25:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=8080)
+    socketio.run(app, debug=True, host='0.0.0.0', port=8080, allow_unsafe_werkzeug=True)
