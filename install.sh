2429719d63ae9282b0d51789efacf9e6cc5fa31e
katy-the-kat <hello@is-a.space>
2024-11-25 03:41:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 apt-get -y install git python3-pip
 git clone https://github.com/katy-the-kat/SpaceRAC.git
 pip3 install flask flask-socketio --break-system-packages
+pip3 install flask flask-socketio
 cd SpaceRAC
